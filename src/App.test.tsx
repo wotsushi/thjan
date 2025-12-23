5e70987e3e108fe70dcc84bda6c404af322e12ff
wotsushi <wotsushi@gmail.com>
2025-12-23 21:53:50 +0900

    wip

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-describe("sample test", () => {
+describe("清算", () => {
+  describe("点数計算", () => {
+    beforeEach(() => {
+      render(<App />);
+    });
+  });
+  describe("連荘", () => {});
+  describe("終局", () => {});
   it("should pass", () => {
     render(<App />);
     expect(screen.getByText("ボーナス")).toBeInTheDocument();
