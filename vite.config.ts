f926e5dd31490b58938602679abebe2d09424f72
wotsushi <wotsushi@gmail.com>
2025-12-23 22:05:43 +0900

    PWA対応

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,34 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "thjan",
+        short_name: "thjan",
+        start_url: "/thjan/",
+        display: "standalone",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "favicon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "favicon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/thjan/",
   test: {
     globals: true,
